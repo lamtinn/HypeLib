bc218872aead724f6389f083b68e5da9eaaf468c
Lam Tin <ngolamtinzk@gmail.com>
2023-09-27 21:20:06 +0700

    Update v1.1.2

--- src/main/java/me/lamtinn/hypelib/event/HypeEvent.java
+++ src/main/java/me/lamtinn/hypelib/event/HypeEvent.java
@@ -1,8 +1,11 @@
 package me.lamtinn.hypelib.event;
 
 import org.bukkit.event.Event;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 public abstract class HypeEvent<E extends Event> implements Listener {
+
+    @EventHandler
     public abstract void onEvent(E e);
 }
